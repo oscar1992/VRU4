0bc0381a75e332f6d20096a79dc379bb9153e113
Oscar Estaban Ramirez Cely <xirious@hotmail.com>
2015-08-02 13:06:21 -0500

    Scripts Faltantes : Oscar E Ramirez Cely

---
+++ Assets/Scripts/CopiarP.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var objetivo:Transform;
+function Start () {
+
+}
+
+function Update () {
+	
+	transform.position.x=objetivo.transform.position.x;
+	transform.position.z=objetivo.transform.position.z;
+}
