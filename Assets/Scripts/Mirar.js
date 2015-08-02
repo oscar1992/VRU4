78e6d11ac3b11354d7d95a9dfae1d585ec8418ca
Oscar Estaban Ramirez Cely <xirious@hotmail.com>
2015-08-01 22:51:43 -0500

    re-comitt scriptes : Oscar Ramirez

---
+++ Assets/Scripts/Mirar.js
@@ -0,0 +1,56 @@
+﻿#pragma strict
+import UI;
+var ray:Ray;
+var apunta:Transform;
+var origen:Vector3;
+var destino:Vector3;
+var hit:RaycastHit;
+var letreo:GameObject;
+var mesh:Image;
+var cubo:GameObject;
+var andar:boolean=false;
+var salir:boolean=true;
+var texto:UI.Text;
+public var Imagenes: Sprite[];
+function Start () {
+	
+}
+
+function Update () {
+	
+	origen=transform.position;
+	destino=transform.TransformDirection(apunta.localPosition);
+	//destino.x=destino.x*10;
+	ray=new Ray(origen, destino);
+	//texto.text=""+andar;
+	if(Physics.Raycast(ray, hit)){
+		var obj=hit.transform.gameObject;
+		print("Toque?: "+andar+"-"+hit.collider.name);	
+		texto.text=""+andar+"-"+hit.collider.name;	
+		letreo=hit.collider.gameObject;
+		if(letreo.tag=="Letrero"&&salir==true){		
+			
+			if(andar==false){
+				andar=true;	
+			}else{
+			 
+				andar=false;
+				
+		
+			}
+			
+			
+			salir=false;
+		}else if(letreo.tag!="Letrero"){			
+			mesh.sprite=Imagenes[1];
+			salir=true;
+		}
+		
+	}
+	if(andar){
+		mesh.sprite=Imagenes[0];
+		cubo.transform.Translate(Vector3.forward*Time.deltaTime);
+	}
+	Debug.DrawRay(ray.origin, destino);
+	
+}
